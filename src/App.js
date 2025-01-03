60e2a422f64bbe127913ed65829bcbb65f3979cd
sarthakp10 <sarthakphadnis10@gmail.com>
2025-01-03 21:21:55 +0530

    NexGen 1.0

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault()
     const contact = { name, email, subject, msg };
-    const response = await fetch('/contact', {
+    const response = await fetch('/api/contact', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
