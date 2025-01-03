3c46b71d8d4ce1bb1540c504bd3fa7efe841eb1e
sarthakp10 <sarthakphadnis10@gmail.com>
2025-01-03 23:37:39 +0530

    NexGen 1.0

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ function App() {
     const contact = { name, email, subject, msg };
     const response = await fetch('https://nexgenapi.vercel.app/api/contact', {
       method: 'POST',
+      mode: 'no-cors',
       headers: {
         'Content-Type': 'application/json'
       },
